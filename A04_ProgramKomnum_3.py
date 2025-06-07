0c7bd2424e06d53d4cf17a6ff50da0c4d1de2032
Freddiee-ux <ferdianardrahafizhan@gmail.com>
2025-06-07 22:30:47 +0700

    Add files via upload

---
+++ A04_ProgramKomnum_3.py
@@ -0,0 +1,38 @@
+def f(x):
+    return x**3 + 10*x**2 - 7*x - 196
+
+def posisi_salah(a, b, xr_true, min_error = 0, max_error = 1, max_iter=100):
+    if f(a) * f(b) >= 0:
+        return None
+ 
+    for i in range(max_iter):
+        xr = b - (f(b) * (a - b)) / (f(a) - f(b))
+        fxr = f(xr)
+
+        if i == 0:
+            error_true = None 
+        else:
+            error_true = abs((xr - xr_true) / xr)
+        
+        if error_true is not None:
+            print(f"Iterasi {i}: a={a:.6f}, b={b:.6f}, xr={xr:.6f}, f(xr)={fxr:.6f}, error_true={error_true:.6f}")
+        else:
+            print(f"Iterasi {i}: a={a:.6f}, b={b:.6f}, xr={xr:.6f}, f(xr)={fxr:.6f}, error_true= N/A")
+
+        if error_true is not None and min_error <= error_true < max_error:
+            return xr
+
+        if f(a) * fxr < 0:
+            b = xr
+        else:
+            a = xr
+
+    return xr
+
+# Contoh penggunaan
+a = -5   
+b = 8
+xr_true = 4
+result = posisi_salah(a, b, xr_true)
+if result is not None:
+    print(f"Akar yang ditemukan: {result:.6f}")
